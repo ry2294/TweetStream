ab07ca53229bb59bc09a9ae744da0113fbfe1e30
Rakesh <ry2294@columbia.edu>
2015-10-17 19:30:26 -0400

    accessing stream data

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.twitter = {};
+
+config.twitter.consumerKey = '';
+config.twitter.consumerSecret = '';
+config.twitter.accessTokenKey = '';
+config.twitter.accessTokenSecret = '';
+
+module.exports = config;
--- tweetStream.js
+++ tweetStream.js
@@ -1,4 +1,27 @@
 var express = require('express');
+var Twitter = require('twitter');
+var config = require('./config');
+
+var client = new Twitter({
+	consumer_key: config.twitter.consumerKey,
+	consumer_secret: config.twitter.consumerSecret,
+	access_token_key: config.twitter.accessTokenKey,
+	access_token_secret: config.twitter.accessTokenSecret
+});
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+client.stream('statuses/filter', {track: 'twitter'},  function(stream){
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
 
 var app = express();
 
